32be3f3bfc1efcba49101f39ce16f430ffbd6a74
daehyeong2 <baconbacon1231@gmail.com>
2024-08-17 15:12:37 +0900

    useQuery (1.2)

--- src/routes/Movies.jsx
+++ src/routes/Movies.jsx
@@ -1,31 +1,27 @@
-import { gql, useApolloClient } from "@apollo/client";
-import { useEffect, useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_MOVIES = gql`
+  query getMovies {
+    allMovies {
+      id
+      title
+    }
+  }
+`;
 
 const Movies = () => {
-  const [movies, setMovies] = useState([]);
-  const client = useApolloClient();
-  useEffect(() => {
-    client
-      .query({
-        query: gql`
-          {
-            allMovies {
-              id
-              title
-            }
-          }
-        `,
-      })
-      .then((res) => setMovies(res.data.allMovies));
-  }, [client]);
+  const { data, loading, error } = useQuery(ALL_MOVIES);
+  if (error) {
+    return <h1>요청에 실패했습니다.</h1>;
+  }
   return (
-    <div>
-      <ul>
-        {movies.map((movie) => (
-          <li key={movie.id}>{movie.title}</li>
-        ))}
-      </ul>
-    </div>
+    <ul>
+      {!loading ? (
+        data.allMovies.map((movie) => <li key={movie.id}>{movie.title}</li>)
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </ul>
   );
 };
 
